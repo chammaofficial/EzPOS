81aebeba67a4e5353a5e56cac083a74704f89fb5
Chamod Priyamal <chammaofficial@gmail.com>
2022-08-13 22:32:23 +0530

    Add project files.

---
+++ EzPOS/UI/Common/FrmLogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EzPOS.UI.Common
+{
+    public partial class FrmLogin : DevExpress.XtraEditors.XtraForm
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
