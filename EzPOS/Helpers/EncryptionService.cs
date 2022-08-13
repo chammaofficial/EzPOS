1c7290555c61b8fdeb097af7b1fd9e3a82e975a4
Chamod Priyamal <chammaofficial@gmail.com>
2022-08-14 00:02:01 +0530

    Added MD5 Services

---
+++ EzPOS/Helpers/EncryptionService.cs
@@ -0,0 +1,11 @@
+﻿
+namespace EzPOS.Helpers
+{
+    public static class EncryptionService
+    {
+        public static string EncryptPassword(string text)
+        {
+            return MD5Hash.Hash.Content(text);
+        }
+    }
+}
