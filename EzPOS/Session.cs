32c373652fb3d9bcd62900170ee48ed485107ce5
Chamod Priyamal <chammaofficial@gmail.com>
2022-08-14 00:06:56 +0530

    Add Session File

---
+++ EzPOS/Session.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EzPOS.Models;
+
+namespace EzPOS
+{
+    public static class Session
+    {
+        public static User LoginUser { get; set; }
+        public static Branch LoginBranch { get; set; }
+    }
+}
